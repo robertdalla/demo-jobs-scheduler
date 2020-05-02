58ae295ae263d9a0d9e426f696d97e4ed9e8d426
robertdalla <robert.dalla@gmail.com>
2020-05-03 05:27:15 +1000

    Default route to Jobs-Scheduler

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,7 +5,7 @@ import { AuthLayoutComponent } from './layouts/auth/auth-layout.component';
 
 export const AppRoutes: Routes = [{
         path: '',
-        redirectTo: 'dashboard',
+        redirectTo: '/pages/jobs-scheduler',
         pathMatch: 'full',
       },{
         path: '',
