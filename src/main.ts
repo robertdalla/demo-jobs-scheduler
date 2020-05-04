7d87c2388ad6c20bf8283682f452f3ae9a8f2d61
robertdalla <robert.dalla@gmail.com>
2020-05-04 20:59:12 +1000

    Ensure Angular destroys itself on hot reloads.

--- src/main.ts
+++ src/main.ts
@@ -22,4 +22,15 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .then(ref => {
+      // Ensure Angular destroys itself on hot reloads.
+      if (window['ngRef']) {
+        window['ngRef'].destroy();
+      }
+      window['ngRef'] = ref;
+
+      // Otherwise, log the boot error
+    })
+    .catch(err => console.error(err));
