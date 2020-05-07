b67d9b35ed40324a706bd97a95f341bd8c407651
robertdalla <robert.dalla@gmail.com>
2020-05-08 06:11:53 +1000

    New UI component datepicker-rangePopup

---
+++ src/app/jobs-scheduler-demo/datepicker-rangePopup.component.ts
@@ -0,0 +1,111 @@
+// angular core
+import {
+    Component,
+    ViewEncapsulation,
+    Input,
+    Output,
+    EventEmitter,
+    ChangeDetectionStrategy
+} from '@angular/core';
+
+// third parties
+import { NgbDate, NgbCalendar, NgbDateParserFormatter } from '@ng-bootstrap/ng-bootstrap';
+
+
+@Component({
+    selector: 'datepicker-range-popup',
+    // template: '',
+    templateUrl: './datepicker-rangePopup.component.html',
+    styles: [`
+        .form-group.hidden {
+            width: 0;
+            margin: 0;
+            border: none;
+            padding: 0;
+        }
+
+        .custom-day {
+            text-align: center;
+            padding: 0.185rem 0.25rem;
+            display: inline-block;
+            height: 2rem;
+            width: 2rem;
+        }
+
+        .custom-day.focused {
+            background-color: #e6e6e6;
+        }
+
+        .custom-day.range, .custom-day:hover {
+            background-color: rgb(2, 117, 216);
+            color: white;
+        }
+
+        .custom-day.faded {
+            background-color: rgba(2, 117, 216, 0.5);
+        }
+    `],
+    // styleUrls: [''],
+    encapsulation: ViewEncapsulation.Emulated,
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+export class DatepickerRangePopupComponent {
+
+    @Input() fromDate: NgbDate | null;
+    @Output() fromDateChange = new EventEmitter();
+
+    @Input() toDate: NgbDate | null;
+    @Output() toDateChange = new EventEmitter();
+
+    hoveredDate: NgbDate | null = null;
+
+
+    constructor(
+        private calendar: NgbCalendar,
+        public formatter: NgbDateParserFormatter
+    ) {
+        this.fromDate = calendar.getToday();
+        this.toDate = calendar.getNext(calendar.getToday(), 'd', 10);
+    }
+
+
+    onDateSelection(date: NgbDate) {
+        if (!this.fromDate && !this.toDate) {
+            this.fromDate = date;
+        } else if (this.fromDate && !this.toDate && date && date.after(this.fromDate)) {
+            this.toDate = date;
+        } else {
+            this.toDate = null;
+            this.fromDate = date;
+        }
+        this.fromDateChange.emit(this.fromDate);
+        this.toDateChange.emit(this.toDate);
+    }
+
+    isHovered(date: NgbDate) {
+        return this.fromDate && !this.toDate && this.hoveredDate && date.after(this.fromDate) && date.before(this.hoveredDate);
+    }
+
+    isInside(date: NgbDate) {
+        return this.toDate && date.after(this.fromDate) && date.before(this.toDate);
+    }
+
+    isRange(date: NgbDate) {
+        return date.equals(this.fromDate) || (this.toDate && date.equals(this.toDate)) || this.isInside(date) || this.isHovered(date);
+    }
+
+    validateFromDateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+        const parsed = this.formatter.parse(input);
+        const value = parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+        this.fromDateChange.emit(value);
+        return value;
+    }
+
+    validateToDateInput(currentValue: NgbDate | null, input: string): NgbDate | null {
+        const parsed = this.formatter.parse(input);
+        const value = parsed && this.calendar.isValid(NgbDate.from(parsed)) ? NgbDate.from(parsed) : currentValue;
+        this.toDateChange.emit(value);
+        return value;
+    }
+}
